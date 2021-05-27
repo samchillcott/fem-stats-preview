31bf123f0ac9bec51551d040cc5a86f90ee04365
Sam Chillcott <samchillcott@gmail.com>
2021-05-27 16:06:50 +0100

    add containers

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Get insights that help your business grow. Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency. 10k+ companies 314 templates 12m+ queries
+      <div className="card">
+        <div className="text-wrapper">
+          <div className="top-text">
+            <h2>
+              Get insights that help your business grow.
+            </h2>
+            <p>
+              Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.
+            </p>
+          </div>
+          <div className="bottom-text">
+            <div>10k+ companies</div>
+            <div>314 templates</div>
+            <div>12m+ queries</div>
+          </div>
+        </div>
+        <div className="image-wrapper">
+          <img src="./assets/images/image-header-desktop.jpg" alt="" />
+        </div>
+        </div>
 
     </div>
   );
