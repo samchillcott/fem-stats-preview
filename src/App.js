6e130a17369f012e7228bb5014b2a6ecc9beb398
Sam Chillcott <samchillcott@gmail.com>
2021-05-27 15:36:10 +0100

    project setup and add assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Get insights that help your business grow. Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency. 10k+ companies 314 templates 12m+ queries
+
     </div>
   );
 }
