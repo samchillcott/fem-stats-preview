fb25042be9f6ac5369c8b51ca3fd647755739572
Sam Chillcott <samchillcott@gmail.com>
2021-05-27 16:58:21 +0100

    add eslint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
@@ -6,9 +7,7 @@ function App() {
       <div className="card">
         <div className="text-wrapper">
           <div className="top-text">
-            <h2>
-              Get insights that help your business grow.
-            </h2>
+            <h2>Get insights that help your business grow</h2>
             <p>
               Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.
             </p>
@@ -19,11 +18,8 @@ function App() {
             <div>12m+ queries</div>
           </div>
         </div>
-        <div className="image-wrapper">
-          <img src="./assets/images/image-header-desktop.jpg" alt="" />
-        </div>
-        </div>
-
+        <div className="image-wrapper">image should be here</div>
+      </div>
     </div>
   );
 }
